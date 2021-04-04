6feec65e6801ebc9db1799915ff219dd7b5c082c
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-04-04 22:21:35 +0300

    utest

--- src/main/java/il/ac/bgu/cs/bp/statespacemapper/RegularExpressionGenerator.java
+++ src/main/java/il/ac/bgu/cs/bp/statespacemapper/RegularExpressionGenerator.java
@@ -25,16 +25,16 @@ public class RegularExpressionGenerator implements Closeable {
       "(?<star_sequence>(?'element_star'){2,})" +
       ")";
   private final static REPattern[] patterns = {
-      new REPattern("({20})=>()", "(?<braces>\\({20}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){20})", "(${braces_body})"),
-      new REPattern("({10})=>()", "(?<braces>\\({10}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){10})", "(${braces_body})"),
-      new REPattern("({5})=>()", "(?<braces>\\({5}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){5})", "(${braces_body})"),
-      new REPattern("({2})=>()", "(?<braces>\\({5}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){2})", "(${braces_body})"),
-      new REPattern("(a()) => ()", "\\(( ? 'element')\\(\\)\\)", " () "),
-      new REPattern("()* => ()", "\\(\\)\\*", "()"),
-      new REPattern("(a) => a", "\\((?'element')\\)", "${element}"),
-      new REPattern("$* => $", "\\$\\*", "\\$"),
-      new REPattern("(a*)* => a*", "\\((?'element')\\*\\)\\*", "${element}*"),
-      new REPattern("(a+b*)* => (a+b)*", "\\((?=(?>(?'any_element')\\+)*(?'element_star'))(?'or_sequence')\\)\\*", new DefaultCaptureReplacer() {
+      new REPattern(0, "({20})=>()", "(?<braces>\\({20}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){20})", "(${braces_body})"),
+      new REPattern(1, "({10})=>()", "(?<braces>\\({10}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){10})", "(${braces_body})"),
+      new REPattern(2, "({5})=>()", "(?<braces>\\({5}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){5})", "(${braces_body})"),
+      new REPattern(3, "({2})=>()", "(?<braces>\\({2}(?<braces_body>(?>[^()]|(?'braces')|(?'brace'))*)\\){2})", "(${braces_body})"),
+      new REPattern(4, "(a()) => ()", "\\(( ? 'element')\\(\\)\\)", " () "),
+      new REPattern(5, "()* => ()", "\\(\\)\\*", "()"),
+      new REPattern(6, "(a) => a", "\\((?'element')\\)", "${element}"),
+      new REPattern(7, "$* => $", "\\$\\*", "\\$"),
+      new REPattern(8, "(a*)* => a*", "\\((?'element')\\*\\)\\*", "${element}*"),
+      new REPattern(9, "(a+b*)* => (a+b)*", "\\((?=(?>(?'any_element')\\+)*(?'element_star'))(?'or_sequence')\\)\\*", new DefaultCaptureReplacer() {
         @Override
         public String replace(CaptureTreeNode node) {
           if ("element_star".equals(node.getGroupName()))
@@ -42,8 +42,8 @@ public class RegularExpressionGenerator implements Closeable {
           return super.replace(node);
         }
       }),
-      new REPattern("$+a* => a*", "\\$\\+(?'element_star')", "${element_star}"),
-      new REPattern("(a*b*)* => (a*+b*)*", "(?<=\\()(?'star_sequence')(?=(?'element_star')\\)\\*)", new DefaultCaptureReplacer() {
+      new REPattern(10, "$+a* => a*", "\\$\\+(?'element_star')", "${element_star}"),
+      new REPattern(11, "(a*b*)* => (a*+b*)*", "(?<=\\()(?'star_sequence')(?=(?'element_star')\\)\\*)", new DefaultCaptureReplacer() {
         @Override
         public String replace(CaptureTreeNode node) {
           if ("element_star".equals(node.getGroupName()))
@@ -51,20 +51,20 @@ public class RegularExpressionGenerator implements Closeable {
           return super.replace(node);
         }
       }),
-      new REPattern("$a => a", "\\$(?'element')", "${element}"),
-      new REPattern("a+a => a", "(?<before>(?>(?'any_element')\\+)*)(?<first>(?'any_element'))\\+(?<after>(?>(?'any_element')\\+){0,}?)\\k<first>(?![^)+])",
+      new REPattern(12, "$a => a", "\\$(?'element')", "${element}"),
+      new REPattern(13, "a+a => a", "(?<before>(?>(?'any_element')\\+)*)(?<first>(?'any_element'))\\+(?<after>(?>(?'any_element')\\+){0,}?)\\k<first>(?![^)+])",
           "${before}${after}${first}"),                     //
-      new REPattern("", "", ""),       // a+a* => a*
-      new REPattern("", "", ""),       // a*a* => a*
-      new REPattern("", "", ""),       // (aa+a)* => (a)*
-      new REPattern("", "", ""),       // (a + $)* => (a)*
-      new REPattern("", "", ""),       // (ab+ac) => a(b+c)
-      new REPattern("", "", ""),       // a*aa* => aa*
-      new REPattern("", "", ""),       // (ab+cb) => (a+c)b
-      new REPattern("", "", ""),       // a*($+b(a+b)*) => (a+b)*
-      new REPattern("", "", ""),       // ($+(a+b)*a)b* => (a+b)*
-      new REPattern("", "", ""),       // ab(cd) => abcd
-      new REPattern("", "", ""),       // (a+(b+c)) => a+b+c
+      new REPattern(14, "", "", ""),       // a+a* => a*
+      new REPattern(15, "", "", ""),       // a*a* => a*
+      new REPattern(16, "", "", ""),       // (aa+a)* => (a)*
+      new REPattern(17, "", "", ""),       // (a + $)* => (a)*
+      new REPattern(18, "", "", ""),       // (ab+ac) => a(b+c)
+      new REPattern(19, "", "", ""),       // a*aa* => aa*
+      new REPattern(20, "", "", ""),       // (ab+cb) => (a+c)b
+      new REPattern(21, "", "", ""),       // a*($+b(a+b)*) => (a+b)*
+      new REPattern(22, "", "", ""),       // ($+(a+b)*a)b* => (a+b)*
+      new REPattern(23, "", "", ""),       // ab(cd) => abcd
+      new REPattern(24, "", "", ""),       // (a+(b+c)) => a+b+c
   };
   private final Context cx;
   private final Scriptable scope;
@@ -96,33 +96,6 @@ public class RegularExpressionGenerator implements Closeable {
     }
   }
 
-  public static void main(String[] args) {
-//    Object[] p = {"(?<before>(?>(?'any_element')\\+)*)(?<first>(?'any_element'))\\+(?<after>(?>(?'any_element')\\+){0,}?)(?=\\k<first>[)+])", "b: ${before}--- a: ${after} --- f: ${first} 000: "/*new DefaultCaptureReplacer() {
-    Object[] p = {"(?<![^(+])(?<before>(?>(?'any_element')\\+)*)(?<first>(?'any_element'))\\+(?<after>(?>(?'any_element')\\+){0,}?)\\k<first>(?![^)+])", "${before}${after}${first}"/*new DefaultCaptureReplacer() {
-//    Object[] p = {"(?'start_element_or')", "--${start_element_or}"/*new DefaultCaptureReplacer() {
-      @Override
-      public String replace(CaptureTreeNode node) {
-        if ("element_star".equals(node.getGroupName()))
-          return super.replace(node)+"+";
-        return super.replace(node);
-      }
-    }*/};
-//    String s = "a$b$t(a$g)a";
-    String s = "aa+z+c+s+a+(b+a)";
-    Matcher m = Pattern.compile(definitions + p[0]).matcher(s);
-    System.out.println("original: " + s);
-    while (m.find()) {
-      System.out.println("group: " + m.group());
-//      System.out.println("body: " + m.group("body"));
-    }
-    String regex2;
-    if (p[1] instanceof String)
-      regex2 = m.replaceAll((String) p[1]);
-    else
-      regex2 = m.replaceAll((CaptureReplacer) p[1]);
-    System.out.println(regex2);
-  }
-
   public String generateRegex() {
     var getRE = (Function) scope.get("getRE", scope);
     regex = (String) getRE.call(cx, scope, scope, new Object[]{noamGraph});
@@ -136,6 +109,10 @@ public class RegularExpressionGenerator implements Closeable {
 
   public static String simplify(String regex, String pattern) {
     return simplify(regex, Arrays.stream(patterns).filter(p->p.name.equals(pattern)).findFirst().orElseThrow());
+  }
+
+  public static String simplify(String regex, int patternId) {
+    return simplify(regex, patterns[patternId]);
   }
 
   public static String simplify(String regex, REPattern pattern) {
@@ -180,17 +157,20 @@ public class RegularExpressionGenerator implements Closeable {
 
 
   static class REPattern {
+    public final int id;
     public final String name;
     public final String pattern;
     public final Object replace;
 
-    REPattern(String name, String pattern, String replace) {
+    REPattern(int id, String name, String pattern, String replace) {
+      this.id = id;
       this.name = name;
       this.pattern = pattern;
       this.replace = replace;
     }
 
-    REPattern(String name, String pattern, DefaultCaptureReplacer replace) {
+    REPattern(int id, String name, String pattern, DefaultCaptureReplacer replace) {
+      this.id = id;
       this.name = name;
       this.pattern = pattern;
       this.replace = replace;
--- src/test/java/il/ac/bgu/cs/bp/statespacemapper/RegularExpressionGeneratorTest.java
+++ src/test/java/il/ac/bgu/cs/bp/statespacemapper/RegularExpressionGeneratorTest.java
@@ -1,16 +1,25 @@
 package il.ac.bgu.cs.bp.statespacemapper;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegularExpressionGeneratorTest {
-  @Test
-  void test2ParanthesesTo1() { //({2})=>()
-    String[][] strings = { {"((a))", "(a)"}};
+  private void test(String pattern, String[][] strings) {
     for (int i = 0; i < strings.length; i++) {
-      assertEquals(strings[i][1],RegularExpressionGenerator.simplify(strings[i][0], "({2})=>()"));
+      assertEquals(strings[i][1],RegularExpressionGenerator.simplify(strings[i][0], pattern));
     }
+  }
+
+  @Test
+  void test3() { //({2})=>()
+    String[][] strings = {
+        {"((a))", "(a)"},
+        {"((aa))", "(aa)"},
+        {"((a*))", "(a*)"},
+        {"((a*+b*))", "(a*+b*)"},
+        {"((a*+b*(a*b*+c(a*b*+c))))", "(a*+b*(a*b*+c(a*b*+c)))"},
+    };
+    test("({2})=>()", strings);
   }
 }
