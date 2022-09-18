4153640e8274b6293908d59ff8074f303dad58d2
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2022-09-18 17:58:26 +0300

    Added examples

--- src/main/java/il/ac/bgu/cs/bp/statespacemapper/GenerateAllTracesInspection.java
+++ src/main/java/il/ac/bgu/cs/bp/statespacemapper/GenerateAllTracesInspection.java
@@ -1,20 +1,24 @@
 package il.ac.bgu.cs.bp.statespacemapper;
 
+import il.ac.bgu.cs.bp.bpjs.BPjs;
 import il.ac.bgu.cs.bp.bpjs.analysis.ExecutionTrace;
 import il.ac.bgu.cs.bp.bpjs.analysis.ExecutionTraceInspection;
 import il.ac.bgu.cs.bp.bpjs.analysis.ExecutionTraceInspections;
 import il.ac.bgu.cs.bp.bpjs.analysis.violations.Violation;
-import il.ac.bgu.cs.bp.bpjs.bprogramio.BProgramSyncSnapshotIO;
+import il.ac.bgu.cs.bp.bpjs.bprogramio.*;
+import il.ac.bgu.cs.bp.bpjs.execution.jsproxy.BProgramJsProxy;
 import il.ac.bgu.cs.bp.bpjs.internal.Pair;
 import il.ac.bgu.cs.bp.bpjs.model.BEvent;
+import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 import il.ac.bgu.cs.bp.bpjs.model.BProgramSyncSnapshot;
 import il.ac.bgu.cs.bp.bpjs.model.BThreadSyncSnapshot;
+import org.mozilla.javascript.Context;
 import org.mozilla.javascript.NativeContinuation;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
@@ -113,6 +117,7 @@ public class GenerateAllTracesInspection implements ExecutionTraceInspection {
   }
 
   public MapperResult getResult() {
+    var dir = "bug-run" + 3;
     var states = Stream.concat(graph.keySet().stream(), graph.values().stream().flatMap(map -> map.keySet().stream())).distinct().collect(Collectors.toUnmodifiableList());
 
     var indexedStates = IntStream.range(0, states.size()).boxed().collect(Collectors.toUnmodifiableMap(states::get, Function.identity()));
@@ -158,23 +163,34 @@ public class GenerateAllTracesInspection implements ExecutionTraceInspection {
               ss2bt = ss2.getBThreadSnapshots().stream().filter(snapshot -> snapshot.getName().equals(bt.getName())).findFirst().get();
             }
           }
-          System.out.println("Name of conflicting b-thread: " + ss1bt.getName());
-          System.out.println("NativeContinuation.equalImplementations(list0bt.getContinuation(),list1bt.getContinuation()) = " + NativeContinuation.equalImplementations(list0bt.getContinuation(), list1bt.getContinuation()));
-          System.out.println("NativeContinuation.equalImplementations(ss1bt.getContinuation(),list0bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss1bt.getContinuation(), list0bt.getContinuation()));
-          System.out.println("NativeContinuation.equalImplementations(ss1bt.getContinuation(),list1bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss1bt.getContinuation(), list1bt.getContinuation()));
-          System.out.println("NativeContinuation.equalImplementations(ss2bt.getContinuation(),list0bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss2bt.getContinuation(), list0bt.getContinuation()));
-          System.out.println("NativeContinuation.equalImplementations(ss2bt.getContinuation(),list1bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss2bt.getContinuation(), list1bt.getContinuation()));
-          System.out.println("listSS1.get(0).equals(ss1) = " + listSS1.get(0).equals(ss1));
-          System.out.println("ss1.equals(listSS1.get(0)) = " + ss1.equals(listSS1.get(0)));
-          System.out.println("listSS1.get(1).equals(ss1) = " + listSS1.get(1).equals(ss1));
-          System.out.println("ss1.equals(listSS1.get(1)) = " + ss1.equals(listSS1.get(1)));
-          System.out.println("listSS1.get(0).equals(ss2) = " + listSS1.get(0).equals(ss2));
-          System.out.println("ss2.equals(listSS1.get(0)) = " + ss2.equals(listSS1.get(0)));
-          System.out.println("listSS1.get(1).equals(ss2) = " + listSS1.get(1).equals(ss2));
-          System.out.println("ss2.equals(listSS1.get(1)) = " + ss2.equals(listSS1.get(1)));
-          writeSnapshotsToFile(listSS1, ss1, ss2);
-          System.exit(1);
+          try (FileWriter fileWriter = new FileWriter(dir + "/stdout.txt", false);
+               PrintWriter out = new PrintWriter(fileWriter);) {
+            out.println("Name of conflicting b-thread: " + ss1bt.getName());
+            out.println("NativeContinuation.equalImplementations(list0bt.getContinuation(),list1bt.getContinuation()) = " + NativeContinuation.equalImplementations(list0bt.getContinuation(), list1bt.getContinuation()));
+            out.println("NativeContinuation.equalImplementations(ss1bt.getContinuation(),list0bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss1bt.getContinuation(), list0bt.getContinuation()));
+            out.println("NativeContinuation.equalImplementations(ss1bt.getContinuation(),list1bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss1bt.getContinuation(), list1bt.getContinuation()));
+            out.println("NativeContinuation.equalImplementations(ss2bt.getContinuation(),list0bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss2bt.getContinuation(), list0bt.getContinuation()));
+            out.println("NativeContinuation.equalImplementations(ss2bt.getContinuation(),list1bt.getContinuation()) = " + NativeContinuation.equalImplementations(ss2bt.getContinuation(), list1bt.getContinuation()));
+            out.println("listSS1.get(0).equals(ss1) = " + listSS1.get(0).equals(ss1));
+            out.println("ss1.equals(listSS1.get(0)) = " + ss1.equals(listSS1.get(0)));
+            out.println("listSS1.get(1).equals(ss1) = " + listSS1.get(1).equals(ss1));
+            out.println("ss1.equals(listSS1.get(1)) = " + ss1.equals(listSS1.get(1)));
+            out.println("listSS1.get(0).equals(ss2) = " + listSS1.get(0).equals(ss2));
+            out.println("ss2.equals(listSS1.get(0)) = " + ss2.equals(listSS1.get(0)));
+            out.println("listSS1.get(1).equals(ss2) = " + listSS1.get(1).equals(ss2));
+            out.println("ss2.equals(listSS1.get(1)) = " + ss2.equals(listSS1.get(1)));
+            out.flush();
+            var bprog = ss1.getBProgram();
+            writeContinuation(bprog, ss1bt, dir+"/ss1bt.bin");
+            writeContinuation(bprog, ss2bt, dir+"/ss2bt.bin");
+            writeContinuation(bprog, list0bt, dir+"/list0bt.bin");
+            writeContinuation(bprog, list1bt, dir+"/list1bt.bin");
+            System.out.println("found bug!");
+            System.exit(1);
 //          }
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
         }
       }
     }
@@ -186,29 +202,42 @@ public class GenerateAllTracesInspection implements ExecutionTraceInspection {
     return new MapperResult(indexedStates, links, traces, startNode, acceptingStatesMap);
   }
 
-  private void writeSnapshotsToFile(List<BProgramSyncSnapshot> states, BProgramSyncSnapshot s1, BProgramSyncSnapshot s2) {
-    System.out.println("Writing files");
+  private static void writeContinuation(BProgram bprog, BThreadSyncSnapshot btss, String filename) {
     try {
-      Files.deleteIfExists(Path.of("snapshots"));
-      Files.createDirectories(Path.of("snapshots"));
-    } catch (IOException e) {
-      throw new RuntimeException("Could not create sanpshots directory",e);
-    }
-    writeSnapshotToFile(s1, "snapshots/bpss1.bin");
-    writeSnapshotToFile(s2, "snapshots/bpss2.bin");
-    for (int i = 0; i < states.size(); i++) {
-      writeSnapshotToFile(states.get(i), "snapshots/states"+i+".bin");
+      BPjs.enterRhinoContext();
+      try (ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+           BPJSStubOutputStream outs = new BPJSStubOutputStream(bytes, bprog.getGlobalScope())) {
+        outs.writeObject(btss.getContinuation());
+        Files.write(Path.of(filename), bytes.toByteArray());
+      } catch (IOException e) {
+        throw new RuntimeException("Failed to clone snapshot: " + e.getMessage(), e);
+      }
+    } finally {
+      Context.exit();
     }
   }
 
-  private void writeSnapshotToFile(BProgramSyncSnapshot s, String name) {
-    BProgramSyncSnapshotIO io = new BProgramSyncSnapshotIO(s.getBProgram());
-    try {
-      try (var file = new FileOutputStream(name)) {
-        file.write(io.serialize(s));
+  private static StubProvider getStubProvider(BProgramSyncSnapshot bprogram) {
+    BProgramJsProxy bpProxy = bprogram.getBProgram().getFromGlobalScope("bp", BProgramJsProxy.class).get();
+    return (stub) -> {
+      if (stub == StreamObjectStub.BP_PROXY) {
+        return bpProxy;
+      } else {
+        throw new IllegalArgumentException("Unknown stub " + stub);
       }
-    } catch (IOException e) {
-      throw new RuntimeException("Failed to serialize snapshot " + name + ": " + e.getMessage(), e);
+    };
+  }
+
+  public static Object deserializeContinuation(BProgramSyncSnapshot bprog, String filename) throws IOException, ClassNotFoundException {
+    try {
+      BPjs.enterRhinoContext();
+      var bytes = Files.readAllBytes(Paths.get(filename));
+      try (var bris = new ByteArrayInputStream(bytes);
+           var sis = new BPJSStubInputStream(bris, bprog.getBProgram().getGlobalScope(), getStubProvider(bprog))) {
+        return sis.readObject();
+      }
+    } finally {
+      Context.exit();
     }
   }
 
---
+++ src/main/java/il/ac/bgu/cs/bp/statespacemapper/LoadBug.java
@@ -0,0 +1,38 @@
+package il.ac.bgu.cs.bp.statespacemapper;
+
+import il.ac.bgu.cs.bp.bpjs.context.ContextBProgram;
+import org.mozilla.javascript.NativeContinuation;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class LoadBug {
+
+  public static void main(String[] args) throws Exception {
+    var id = 3;
+    var dir = "bug-run" + id;
+    System.out.println("//reading continuations from '" + dir + "'");
+    var bprog = new ContextBProgram("HotCold/dal.js", "HotCold/bl.js").setup();
+    var ss1bt = (NativeContinuation) GenerateAllTracesInspection.deserializeContinuation(bprog, dir+"/ss1bt.bin");
+    var ss2bt = (NativeContinuation) GenerateAllTracesInspection.deserializeContinuation(bprog, dir+"/ss2bt.bin");
+    var list0bt = (NativeContinuation) GenerateAllTracesInspection.deserializeContinuation(bprog, dir+"/list0bt.bin");
+    var list1bt = (NativeContinuation) GenerateAllTracesInspection.deserializeContinuation(bprog, dir+"/list1bt.bin");
+
+    try (FileWriter fileWriter = new FileWriter(dir + "/stdout.txt", true);
+         PrintWriter out = new PrintWriter(fileWriter);) {
+      out.println();
+      out.println();
+      out.println();
+      out.println("now deserialize...");
+      out.println();
+      out.println("NativeContinuation.equalImplementations(list0bt,list1bt) = " + NativeContinuation.equalImplementations(list0bt, list1bt));
+      out.println("NativeContinuation.equalImplementations(ss1bt,list0bt) = " + NativeContinuation.equalImplementations(ss1bt, list0bt));
+      out.println("NativeContinuation.equalImplementations(ss1bt,list1bt) = " + NativeContinuation.equalImplementations(ss1bt, list1bt));
+      out.println("NativeContinuation.equalImplementations(ss2bt,list0bt) = " + NativeContinuation.equalImplementations(ss2bt, list0bt));
+      out.println("NativeContinuation.equalImplementations(ss2bt,list1bt) = " + NativeContinuation.equalImplementations(ss2bt, list1bt));
+      out.flush();
+    }
+  }
+}
